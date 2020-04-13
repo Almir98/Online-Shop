0fd4d866b7dc32f0f3460c5b1cfe5ef1ca475182
Almir98 <almir.tihak@edu.fit.ba>
2020-04-14 00:03:55 +0200

    Dorada

---
+++ OnlineShop/OnlineShop/ViewModels/ShowOrdersVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.ViewModels
+{
+    public class ShowOrdersVM
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime ShipTime { get; set; }
+        public int UserID { get; set; }  //za pregled profila, napravit nesto tipa /ShowInfo?userid=666 cisto da admin ima uvida u tog korisnika, njegove informacije neke
+        public string UserInfo { get; set; } //u stringu spojeno ime prezime i adresa sa brojem telefona
+        public List<Rows> Items { get; set; }
+        public class Rows{
+            public string ProductName { get; set; }
+            public int Quantity { get; set; }
+        }
+        public double TotalPrice { get; set; }
+        public bool IsShipped { get; set; }
+    }
+
+}
---
+++ OnlineShop/OnlineShopPodaci/Migrations/20200413040647_VariableTypeChange.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShopPodaci.Migrations
+{
+    public partial class VariableTypeChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalPrice",
+                table: "order",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsShipped",
+                table: "order",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsShipped",
+                table: "order");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalPrice",
+                table: "order",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
